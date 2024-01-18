c9a4bdff1de7ae42f559a2cd2f807559458bcc8f
Sharofiddin <qoziyevsharofiddin199805@gmail.com>
2024-01-18 08:24:44 +0000

    It has changed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,38 +1,41 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\About;
-use App\Models\Post;
-use App\Models\Video;
-use App\Models\Image;
-use App\Models\Announcement;
-
-use Illuminate\Http\Request;
+use App\Service\AboutService;
+use App\Service\AnnouncementService;
+use App\Service\ImageService;
+use App\Service\PostService;
+use App\Service\VideoService;
 
 class HomeController extends Controller
 {
-    // public function laymain(){
-    //     return view('components.layouts.main')->with([
-    //         'categories' => Category::all(),
-    //     ]);
-    // }
+    public function __construct(
+    protected PostService $postService,
+    protected VideoService $videoService,
+    protected ImageService $imageService,
+    protected AboutService $aboutService,
+    protected AnnouncementService $announcementService,
+    )
+    {
 
+    }
     public function main(){
+        $posts = $this->postService->getByPaginate(10);
+        $videos = $this->videoService->getByPaginate(10);
+        $images = $this->imageService->getByPaginate(10);
+        $announcements = $this->announcementService->getByPaginate(10);
         return view('main.home')->with([
-            'posts' => Post::all(),
-            'videos' => Video::all(),
-            'images' => Image::all(),
-            'announcements' => Announcement::all(),
+            'posts' => $posts,
+            'videos' => $videos,
+            'images' => $images,
+            'announcements' => $announcements,
         ]);
     }
-
     public function contact(){
         return view('main.contact');
     }
-
     public function about(){
-        return view('main.institute')->with([
-            'abouts' => About::all(),
-        ]);
+        $abouts = $this->aboutService->getByPaginate(10);
+        return view('main.institute')->with(['abouts' => $abouts]);
     }
 }
